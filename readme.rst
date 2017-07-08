1194369da3ec9ebb06cf214196c10d32baa8bb31
russellballestrini <russell.ballestrini@gmail.com>
2017-07-08 18:52:48 -0400

    first commit! 	new file:   .gitignore 	new file:   MANIFEST.in 	new file:   readme.rst 	new file:   remarkbox_theme_meta/__init__.py 	new file:   remarkbox_theme_meta/static/theme/meta/css/custom.css 	new file:   remarkbox_theme_meta/static/theme/meta/css/normalize.css 	new file:   remarkbox_theme_meta/static/theme/meta/css/skeleton.css 	new file:   remarkbox_theme_meta/static/theme/meta/img/remarkbox-minified.png 	new file:   remarkbox_theme_meta/templates/meta-base.j2 	new file:   setup.py

---
+++ readme.rst
@@ -0,0 +1,22 @@
+Remarkbox theme for meta and faq
+################################
+
+This is the theme used on:
+
+* https://meta.remarkbox.com
+* https://faq.remarkbox.com
+* https://demo.remarkbox.com
+
+If you use a hosted site you may choose this theme on your namespace dashboard.
+
+Otherwise a dedicated Remarkbox must install the theme into the python environment using:
+
+.. code-block:: bash
+
+  python setup.py install
+  
+Then configure it in the Remarkbox .ini file using:
+
+.. code-block:: bash
+
+  app.theme = meta
---
+++ setup.py
@@ -0,0 +1,32 @@
+# A Remarkbox theme is a python package with the following paths:
+# 
+#   template/<theme_name>-base.j2
+#   static/theme/<theme_name>
+#
+# The theme needs to be registered to the 'remarkbox.themes'
+# entry_point group to make it loaded by the Remarkbox application.
+
+from setuptools import setup, find_packages
+
+setup(name='remarkbox_theme_meta',
+      version='0.2.0',
+      description='remarkbox theme for meta and faq',
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Russell Ballestrini',
+      author_email='russell@ballestrini.net',
+      url='http://russell.ballestrini.net',
+      keywords='remarkbox theme for meta and faq',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points = {
+        'remarkbox.themes' : [
+            'meta = remarkbox_theme_meta'
+        ],
+      },
+)
