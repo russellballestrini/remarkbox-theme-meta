d16b9a465617f8996dabcf42f6f75024eb614410
russellballestrini <russell.ballestrini@gmail.com>
2018-05-24 15:00:49 -0400

    modified:   remarkbox_theme_meta/static/theme/meta/css/diff.css 	modified:   remarkbox_theme_meta/static/theme/meta/css/meta.css 	modified:   remarkbox_theme_meta/templates/meta-base.j2 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # A Remarkbox theme is a python package with the following paths:
-# 
+#
 #   template/<theme_name>-base.j2
 #   static/theme/<theme_name>
 #
@@ -8,25 +8,22 @@
 
 from setuptools import setup, find_packages
 
-setup(name='remarkbox_theme_meta',
-      version='0.2.1',
-      description='remarkbox theme for meta and faq',
-      classifiers=[
+setup(
+    name="remarkbox_theme_meta",
+    version="0.2.1",
+    description="remarkbox theme for meta and faq",
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='Russell Ballestrini',
-      author_email='russell@ballestrini.net',
-      url='http://russell.ballestrini.net',
-      keywords='remarkbox theme for meta and faq',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      entry_points = {
-        'remarkbox.themes' : [
-            'meta = remarkbox_theme_meta'
-        ],
-      },
+    ],
+    author="Russell Ballestrini",
+    author_email="russell@ballestrini.net",
+    url="http://russell.ballestrini.net",
+    keywords="remarkbox theme for meta and faq",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"remarkbox.themes": ["meta = remarkbox_theme_meta"]},
 )
